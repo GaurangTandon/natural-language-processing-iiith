738a2b095ede95320b9edb1687edf7ea7a98c7ab
Animesh Sinha <animeshsinha1309@gmail.com>
2019-04-03 19:55:24 +0530

    Started off the Python Port
    Created a new Django project and a app for the Chunking lab. Also added in a couple of models.

---
+++ src/VlabsNLP/chunking/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Sentence(models.Model):
+    languages = ( ('ENG', 'ENGLISH'), ('HIN', 'Hindi') )
+    sentence = models.CharField(max_length=200)
+    language = models.CharField(max_length=3, choices=languages)
+
+    def __str__(self):
+        return self.sentence
+
+class Chunk(models.Model):
+    pos_options = ( ('NNP', 'NNP'), ('VB', 'VB') )
+    chunk_options = ( ('B-NP', 'B-NP'), ('B-VP', 'B-VP') )
+    sentence = models.ForeignKey('Sentence', on_delete=models.CASCADE)
+    pos = models.CharField(max_length=10, choices=pos_options)
+    chunk = models.CharField(max_length=10, choices=chunk_options)
